576bf3e694f151fefa36073f44b109089370b459
JoseDiaz <jldi1002@hotmail.com>
2025-03-26 19:56:37 +0100

    subir archivo de texto

--- src/Ejercicio3/Main.java
+++ src/Ejercicio3/Main.java
@@ -1,5 +1,13 @@
 package Ejercicio3;
-
+/*
+Dado el archivo countrties.txt (revisa el apartado recursos) que contiene países y capitales.
+El programa debe leer el archivo y guardar los datos en un HashMap<String, String>.
+El programa pide el nombre del usuario/a, y después debe mostrar un país de forma
+aleatoria, guardado en HashMap. Se trata de que el usuario debe escribir el nombre
+de la capital del país en cuestión. Si acierta se le suma un punto. Esta acción se
+repite 10 veces. Una vez solicitadas las capitales de 10 países de forma aleatoria, entonces
+debe guardarse en un fichero llamado classificacio.txt, el nombre del usuario y su puntuación.
+*/
 public class Main {
     public static void main(String[] args) {
 
