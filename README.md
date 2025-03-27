cf7a01d6d289399268cbc8b317a5205f1ea13b54
JoseDiaz <jldi1002@hotmail.com>
2025-03-27 11:02:35 +0100

    haciendo un pull en la academy

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-01.03-JavaColletions-Nivel1
+ 01.03-JavaColletions-Nivel1
 Statement: 
-Exercice 1
+#Exercice 1
 Create a class named `Month` with an attribute `"name"` (which will store the name of the month of the year).  
 Add 11 `Month` objects (each with a different attribute) to an `ArrayList`, except for the object with the attribute `"Agost"`. 
 Then, insert it in the correct position corresponding to this month and demonstrate that the `ArrayList` maintains the correct order.
--- src/Ejercicio3/ReadFile.java
+++ src/Ejercicio3/ReadFile.java
@@ -4,18 +4,19 @@ import java.io.*;
 
 public class ReadFile {
     public void read (){
+        char letter = 0;
         try {
-            FileReader inpu = new FileReader("C:/Users/Windows10Pro/IdeaProjects/01.03-JavaCollections-nivel1/countries.txt");
+            FileReader inpu = new FileReader("C:\\Users\\formacio\\Desktop\\JoseProgramas\\01.03-JavaCollections-nivel1/countries.txt");
             int x = 0;
             while (x!=-1){
                 x = inpu.read();
-                char letter = (char)x;
+                letter = (char)x;
                 System.out.print(letter);
             }
             inpu.close();
         } catch (IOException e) {
             //throw new RuntimeException(e);
-            System.out.println("file not found");
+            //System.out.println("file not found");
         }
     }
 }
